7e60a1493379964967ce5ca9146b0dc95731e4c9
Prashant <blackhole.gamma@gmail.com>
2020-05-22 16:40:57 +0530

    Updates

---
+++ precache-manifest.305ff566d89c91b7c5e2f608d92fa93d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87bf7da7249252a83a5c3ba6431fa12",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70592218c36c59f9c191",
+    "url": "/portfolio/static/js/2.0b5a06b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.0b5a06b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f0afa0be34fd08e988",
+    "url": "/portfolio/static/js/main.34a0444d.chunk.js"
+  },
+  {
+    "revision": "37949c9ed895f2bbbd8a",
+    "url": "/portfolio/static/js/runtime-main.389b6903.js"
+  },
+  {
+    "revision": "22ff146a632db9f5cb893b9ef024ba77",
+    "url": "/portfolio/static/media/contact.22ff146a.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "32bc428f67abce6b369e0d6cf224f28a",
+    "url": "/portfolio/static/media/graphql.32bc428f.svg"
+  },
+  {
+    "revision": "9f2e1637c83e37e8f555044e12324734",
+    "url": "/portfolio/static/media/html5.9f2e1637.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolio/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "2fadd2a137085081faa798150fee4a62",
+    "url": "/portfolio/static/media/materialui.2fadd2a1.svg"
+  },
+  {
+    "revision": "554c7d4bfd63a3da33773978fd291c06",
+    "url": "/portfolio/static/media/mongodb.554c7d4b.svg"
+  },
+  {
+    "revision": "5c376133d6403088a87a04fcc9685674",
+    "url": "/portfolio/static/media/nodejs.5c376133.svg"
+  },
+  {
+    "revision": "900feec7f44aff14f0ccc79ab5a5da21",
+    "url": "/portfolio/static/media/profileImage.900feec7.jpg"
+  },
+  {
+    "revision": "6bc8ab29acf42a58d51d7c22f57ae749",
+    "url": "/portfolio/static/media/programmer.6bc8ab29.svg"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/portfolio/static/media/reactjs.70384772.svg"
+  },
+  {
+    "revision": "b4477375cbcd42acb9cb627611390dbf",
+    "url": "/portfolio/static/media/redis.b4477375.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "ce0fd9cb13a3e63c5b680c9579d47df5",
+    "url": "/portfolio/static/media/sql.ce0fd9cb.svg"
+  },
+  {
+    "revision": "3aeee2292aa7770ccf0aec8e43bdaaf5",
+    "url": "/portfolio/static/media/typescript.3aeee229.svg"
+  }
+]);
